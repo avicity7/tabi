b0058d4a18abae2ffad40a6f476303a339c2703d
avicity7 <74177090+avicity7@users.noreply.github.com>
2023-03-06 23:39:11 +0800

    fix: Mobile width fix, again

--- pages/index.tsx
+++ pages/index.tsx
@@ -73,7 +73,7 @@ const Home = (props) => {
             <li key={journey.id}>
                 <div className="grid place-items-center font-DMSans">
                   <button onClick={() => {pushToJourney(journey.id)}}>
-                    <Card minW={{base:'sm',md:'lg'}} maxW={'lg'} className = "my-5 mx-5" overflow="hidden">
+                    <Card minW={{base:'sm',md:'lg'}} maxW={{base:'sm',md:'lg'}} className = "my-5 mx-5" overflow="hidden">
                       <Image objectFit='fill' src='https://www.timetravelturtle.com/wp-content/uploads/2018/11/Tokyo-2018-280_feat1.jpg' alt='tokyo'/>
                       <CardBody>
                         <Stack spacing='3'>
